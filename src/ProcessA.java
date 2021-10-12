214b618dd631518a56635c60db0ba8a97b2c1834
kimbielby <kib00075@students.stir.ac.uk>
2021-10-12 11:00:43 +0100

    Notes explaining why code is example of deadlock added.

--- src/ProcessA.java
+++ src/ProcessA.java
@@ -20,5 +20,11 @@ public class ProcessA {
         catch (InterruptedException e){}
         allSemaphs.v(sem2);
         allSemaphs.v(sem1);
+
+        // Deadlock has been achieved
+        // 1. Mutual exclusion: Each Semaphore only has 1 permit, so only one process can have it at a time
+        // 2. Hold and wait: Each process can request as many Semaphores as they like, providing there is an available permit
+        // 3. No preemption: Due to the acquire() method another process must wait until the permit for that particular permit is released
+        // 4. Circular wait: There are two processes trying to get both Semaphores 
     }
 }
